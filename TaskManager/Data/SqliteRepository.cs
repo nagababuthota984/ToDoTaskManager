ccc80969011f02b322768f8a91fbb3d5413b6965
Thota Nagababu <nagababuthota984@outlook.com>
2022-07-06 21:05:39 +0530

    applied code first approach and created sqlite db

--- TaskManager/Data/SqliteRepository.cs
+++ TaskManager/Data/SqliteRepository.cs
@@ -11,36 +11,30 @@ namespace TaskManager.Data
 {
     public class SqliteRepository : ITaskRepository
     {
-        private readonly TaskManagerDbContext _context;
-        private readonly IMapper _mapper;
+        private readonly SQLiteDbContext _context;
 
-        public SqliteRepository(TaskManagerDbContext context, IMapper mapper)
+        public SqliteRepository(SQLiteDbContext context)
         {
             _context = context;
-            _mapper = mapper;
         }
         public void CreateTask(TaskDisplayModel task)
         {
-            _context.Tasks.Add(_mapper.Map<SQLite.Task>(task));
+            _context.Tasks.Add(task);
             _context.SaveChanges();
         }
 
         public void DeleteTaskById(Guid id)
         {
-            _context.Tasks.FirstOrDefault(tsk => tsk.Id == id.ToString()).IsDeleted = 1;
+            _context.Tasks.FirstOrDefault(tsk => tsk.Id == id).IsDeleted = true;
             _context.SaveChanges();
         }
 
         public List<TaskDisplayModel> GetAllTasks()
         {
-            return _mapper.Map<List<TaskDisplayModel>>(_context.Tasks);
+            return _context.Tasks.ToList();
         }
-
         public void UpdateTask(TaskDisplayModel taskChanges)
         {
-            var task = _mapper.Map<SQLite.Task>(taskChanges);
-            _context.Tasks.Remove(_context.Tasks.FirstOrDefault(tsk => tsk.Id == task.Id));
-            _context.Tasks.Add(task);
             _context.SaveChanges();
         }
     }
