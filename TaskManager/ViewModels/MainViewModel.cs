29ad94d91a9fc481bb817144a7156f8f9eb2702e
Thota Nagababu <nagababuthota984@outlook.com>
2022-07-08 15:38:04 +0530

    refactored logic for db change functionality

--- TaskManager/ViewModels/MainViewModel.cs
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using TaskManager.Common;
 using TaskManager.Data;
 
@@ -13,6 +14,7 @@ namespace TaskManager.ViewModels
         #region Fields
         private readonly SimpleContainer _container;
         private string _dbProviderName;
+        private static bool handleSelectionChangedEvent = true;
         #endregion
 
         #region Properties
@@ -24,7 +26,7 @@ namespace TaskManager.ViewModels
         {
             get { return _dbProviderName; }
             set { _dbProviderName = value; NotifyOfPropertyChange(nameof(DatabaseProviderName)); }
-        } 
+        }
         #endregion
         public MainViewModel(SimpleContainer container)
         {
@@ -36,18 +38,27 @@ namespace TaskManager.ViewModels
 
         public async void ChangeDb()
         {
-            switch (DatabaseProviderName)
+            if (handleSelectionChangedEvent)
             {
-                case MessageStrings.Sqlserver:
-                    await (Application.Current.MainWindow as MetroWindow).ShowMessageAsync(MessageStrings.ChangeDbTitleMsg, $"{MessageStrings.DbChangeResultsIn}{MessageStrings.Sqlserver} database. {MessageStrings.ConfirmChangeDb}", MessageDialogStyle.Affirmative);
-                        Application.Current.Properties[MessageStrings.Database] = MessageStrings.Sqlserver;
-                    break;
-                default:
-                    await (Application.Current.MainWindow as MetroWindow).ShowMessageAsync(MessageStrings.ChangeDbTitleMsg, $"{MessageStrings.DbChangeResultsIn}{MessageStrings.Sqlite} database. {MessageStrings.ConfirmChangeDb}", MessageDialogStyle.Affirmative);
-                        Application.Current.Properties[MessageStrings.Database] = MessageStrings.Sqlite;
-                    break;
+                handleSelectionChangedEvent = false;
+                switch (DatabaseProviderName)
+                {
+                    case MessageStrings.Sqlserver:
+                        if (MessageDialogResult.Affirmative == await (Application.Current.MainWindow as MetroWindow).ShowMessageAsync(MessageStrings.ChangeDbTitleMsg, $"{MessageStrings.DbChangeResultsIn}{MessageStrings.Sqlserver} database. {MessageStrings.ConfirmChangeDb}", MessageDialogStyle.AffirmativeAndNegative))
+                            Application.Current.Properties[MessageStrings.Database] = MessageStrings.Sqlserver;
+                        else
+                            DatabaseProviderName = MessageStrings.Sqlite;
+                        break;
+                    default:
+                        if (MessageDialogResult.Affirmative == await (Application.Current.MainWindow as MetroWindow).ShowMessageAsync(MessageStrings.ChangeDbTitleMsg, $"{MessageStrings.DbChangeResultsIn}{MessageStrings.Sqlite} database. {MessageStrings.ConfirmChangeDb}", MessageDialogStyle.AffirmativeAndNegative))
+                            Application.Current.Properties[MessageStrings.Database] = MessageStrings.Sqlite;
+                        else
+                            DatabaseProviderName = MessageStrings.Sqlserver;
+                        break;
+                }
+                DisplayHomeView(); 
+                handleSelectionChangedEvent = true;
             }
-            DisplayHomeView();
         }
         public void DisplayHomeView()
         {
