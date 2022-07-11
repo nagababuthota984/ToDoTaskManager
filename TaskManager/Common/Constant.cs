59333fe8a788d1b5b53c09843dd4a1007f9a1a77
Thota Nagababu <nagababuthota984@outlook.com>
2022-07-11 08:05:57 +0530

    messagestrings -> constant

---
+++ TaskManager/Common/Constant.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskManager.Common
+{
+    public static class Constant
+    {
+        public const string Create = "Create";
+        public const string Update = "Update";
+        public const string ConfirmDeleteMsg = "Are you sure you want to delete the task?";
+        public const string ConfirmDeleteWinTitle = "Delete task?";
+        public const string DeleteFailedMsg = "Couldn't delete the task. Please try again";
+        public const string DeleteFailedWinTitle = "Delete Unsuccessful";
+        public const string Database = "Database";
+        public const string Sqlite = "Sqlite";
+        public const string Sqlserver = "Sqlserver";
+        public const string ChangeDbTitleMsg = "Change database";
+        public const string DbChangeResultsIn = "This action results in switching to ";
+        public const string ConfirmChangeDb = "Are you sure you want to change the database?";
+        public const string Today = "Today";
+        public const string Tomorrow = "Tomorrow";
+
+
+
+    }
+}
