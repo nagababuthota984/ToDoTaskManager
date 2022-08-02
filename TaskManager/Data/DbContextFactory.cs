e33a7032694c17ed165c5438f89822f24a8557a3
Nagababu Thota <nagababu.t@Technovert.com>
2022-08-02 16:51:05 +0530

    added mysql online db connection string

--- TaskManager/Data/DbContextFactory.cs
+++ TaskManager/Data/DbContextFactory.cs
@@ -5,13 +5,15 @@ using TaskManager.Data.SqlServer;
 
 namespace TaskManager.Data
 {
-    public class DbContextFactory
+    public static class DbContextFactory
     {
-        public static string sqliteConnectionString = $"Data Source={Constant.ProjectDirectory}\\TaskManager\\Data\\SQLite\\TaskManager.db";
+        private static string sqliteConnectionString = $"Data Source={Constant.ProjectDirectory}\\TaskManager\\Data\\SQLite\\TaskManager.db";
         public const string sqlServerConnectionString = @"Data Source=NAG-HP\SQLEXPRESS;Initial Catalog=TaskManagerDb;Integrated Security=True";
-
+        public const string mySqlConnectionString = "Server=zjicup8yxnhs.us-east-4.psdb.cloud;Database=taskmanager;user=ay8x8ftpmaov;password=pscale_pw_EEDdu6AjXngK0eUP4QAb4E4QcFELjSpp0T_6y0p4MZQ;SslMode=VerifyFull;";
+        
         public static ITaskRepository TaskRepository { get; set; }
 
+        
 
         public static SQLiteDbContext GetSQLiteDbContext()
         {
