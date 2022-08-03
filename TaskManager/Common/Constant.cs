7bc201745a26c5c22dcd6ce7a937ed0a8957eb11
Nagababu Thota <nagababu.t@Technovert.com>
2022-08-03 21:45:52 +0530

    added progress ring while switching db

--- TaskManager/Common/Constant.cs
+++ TaskManager/Common/Constant.cs
@@ -32,7 +32,8 @@ namespace TaskManager.Common
         public const string ProgId = "ProgId";
         public const string IEBrowserRegistryKeyName = @"HKEY_CLASSES_ROOT\https\shell\open\command";
         public static string ProjectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
-
+        public const string NoActiveInternet = "Make sure you have active internet connection";
+        public const string AzureConnectionFailed = "Unable to connect to azure services";
 
     }
 }
